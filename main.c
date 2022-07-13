3931e57461e0c62b2a6ac4f0e3b388413d8db709
Itamar Turner-Trauring <itamar@pythonspeed.com>
2022-07-13 13:22:58 -0400

    Initial setup, a binary that crashes.

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main() {
+  printf("C is an excellent programming language which you should use for all your software.\n");
+  char *string = "A string";
+  string[0] = 'b';
+  printf("See!");
+}
